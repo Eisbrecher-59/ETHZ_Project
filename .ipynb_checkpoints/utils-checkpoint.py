cf72d5ecb0f198263e85c184dff792e8ecfc7ad4
Eisbrecher-59 <sebgillard@gmail.com>
2022-05-23 17:36:08 +0200

    2022-05-16

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -13,13 +13,22 @@ from scipy.stats import bernoulli
 import scipy.linalg as la
 import scipy.sparse as sparse
 import scipy.sparse.linalg as sla
+#------------------------------------------------------------------------------
+def rankorder(x):
+#   """
 
+#   Parameters
+#   ----------   
+    
+#   :param list x: list of int
 
-# from networkx.algorithms.centrality.betweenness import (
-#     _single_source_dijkstra_path_basic as dijkstra,
-# )
-# from networkx.algorithms.centrality.betweenness import (
-#     _single_source_shortest_path_basic as shortest_path,
-# )
-
-# __all__ = ["percolation_centrality"]
+#   Returns
+#   -------
+#   :returns: Rankordering, i.e. descending list of occured values 
+#   :rtype: list(int), list(int)    
+    
+    x1 = list(np.sort(x))
+    x1.reverse()
+    y1 = range(1,len(x1)+1)
+    return np.array(x1),np.array(y1)
+#------------------------------------------------------------------------------
